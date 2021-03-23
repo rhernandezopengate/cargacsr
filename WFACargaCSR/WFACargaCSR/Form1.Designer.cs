36b30dceb984f3230153191603b1128e2e33ac5a
desarrollosir <67165977+desarrollosir@users.noreply.github.com>
2021-03-23 15:44:39 -0600

    Primer Commit

---
+++ WFACargaCSR/WFACargaCSR/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿
+namespace WFACargaCSR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnArbrirArchivo = new System.Windows.Forms.Button();
+            this.txtArchivo = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnArbrirArchivo
+            // 
+            this.btnArbrirArchivo.Location = new System.Drawing.Point(534, 54);
+            this.btnArbrirArchivo.Name = "btnArbrirArchivo";
+            this.btnArbrirArchivo.Size = new System.Drawing.Size(117, 23);
+            this.btnArbrirArchivo.TabIndex = 0;
+            this.btnArbrirArchivo.Text = "Seleccionar Archivo";
+            this.btnArbrirArchivo.UseVisualStyleBackColor = true;
+            this.btnArbrirArchivo.Click += new System.EventHandler(this.btnArbrirArchivo_Click);
+            // 
+            // txtArchivo
+            // 
+            this.txtArchivo.Location = new System.Drawing.Point(261, 57);
+            this.txtArchivo.Name = "txtArchivo";
+            this.txtArchivo.Size = new System.Drawing.Size(267, 20);
+            this.txtArchivo.TabIndex = 1;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 174);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 264);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(261, 97);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(267, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtArchivo);
+            this.Controls.Add(this.btnArbrirArchivo);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnArbrirArchivo;
+        private System.Windows.Forms.TextBox txtArchivo;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ WFACargaCSR/WFACargaCSR/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WFACargaCSR.Entities;
+
+namespace WFACargaCSR
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        DB_A3F19C_OGEntities db = new DB_A3F19C_OGEntities();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnArbrirArchivo_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            txtArchivo.Text = openFileDialog1.FileName;
+            MostrarInformacion(txtArchivo.Text);
+        }
+
+        private void MostrarInformacion(string filePath)
+        {
+            try
+            {
+                DataTable dt = new DataTable();
+
+                string[] lineas = System.IO.File.ReadAllLines(filePath);
+
+                if (lineas.Length > 0)
+                {
+                    //Archivos Con Encabezado
+                    string primeraLinea = lineas[0];
+                    string[] encabezados = primeraLinea.Split(',');
+                    foreach (string headerWord in encabezados)
+                    {
+
+                        if (headerWord.Contains("Piezas"))
+                        {
+                            dt.Columns.Add(new DataColumn(headerWord, typeof(int)));
+                        }
+                        else if (headerWord.Contains("Fecha Recolección "))
+                        {
+                            dt.Columns.Add(new DataColumn(headerWord, typeof(DateTime)));
+                        }
+                        else
+                        {
+                            dt.Columns.Add(new DataColumn(headerWord));
+                        }
+                    }
+
+                    //For Data
+                    for (int i = 1; i < lineas.Length; i++)
+                    {
+                        string[] dataWords = lineas[i].Split(',');
+                        DataRow dr = dt.NewRow();
+                        int columnIndex = 0;
+                        foreach (string palabraEncabezado in encabezados)
+                        {
+                            dr[palabraEncabezado] = dataWords[columnIndex++];
+                        }
+                        dt.Rows.Add(dr);
+                    }
+
+                    progressBar1.Increment(5);
+                    DataTable primeravalidacion = ValidacionPorCuenta(dt);
+                    progressBar1.Increment(20);
+                    DataTable segundavalidacion = ValidacionReferencia(primeravalidacion);
+                    progressBar1.Increment(25);
+                    DataTable terceravalicaion = ActualizacionRegistros(segundavalidacion);
+                    progressBar1.Increment(25);
+                    AgregarRegistrosNuevo(terceravalicaion);
+                    progressBar1.Increment(25);
+
+                }
+                
+                
+                if (dt.Rows.Count > 0)
+                {
+                    dataGridView1.DataSource = dt;
+                }
+
+                MessageBox.Show("Se ha completado el proceso.");
+            }
+            catch (Exception _ex)
+            {
+                MessageBox.Show("Ha ocurrido el siguiente error: " + _ex.Message.ToString());
+            }
+        }
+
+        public DataTable ValidacionPorCuenta(DataTable dt) 
+        {
+            try
+            {
+                int filas = dt.Rows.Count;
+
+                string cuenta = "980522264";
+
+                foreach (DataRow orow in dt.Select()) 
+                {
+                    string numerocuenta = orow["No de Cuenta"].ToString();
+
+                    if (!numerocuenta.Contains(cuenta)) 
+                    {
+                        dt.Rows.Remove(orow);
+                    }
+                }
+
+                //lblValidacionCuenta.Text = "Validacion de cuentas terminada";
+
+                int filasrestantes = dt.Rows.Count;
+                                    
+                dt.AcceptChanges();
+
+                return dt;
+            }
+            catch (Exception _ex)
+            {
+                MessageBox.Show("Ha ocurrido un error: " + _ex.Message.ToString());
+                throw;
+            }            
+        }
+
+        public DataTable ValidacionReferencia(DataTable dt) 
+        {
+            try
+            {
+                int filas = dt.Rows.Count;
+
+                string referencia1 = "KITROPAP";
+                string referencia2 = "GIVEAWAY";
+                string referencia3 = "2Z";
+                string referencia4 = "2T";
+                string referencia5 = "50H";
+                string referencia6 = "QRO";
+
+                foreach (DataRow orow in dt.Select())
+                {
+                    string referencia = orow["Referencia"].ToString();
+                    
+                    if (referencia.Contains(referencia1) ||
+                        referencia.Contains(referencia2) ||
+                        referencia.Contains(referencia3) ||
+                        referencia.Contains(referencia4) ||
+                        referencia.Contains(referencia5) ||
+                        referencia.Contains(referencia6))
+                    {
+                        
+                    }
+                    else
+                    {
+                        dt.Rows.Remove(orow);
+                    }
+                }
+
+                dt.AcceptChanges();
+
+                int filasRestante = dt.Rows.Count;                
+
+                return dt;
+            }
+            catch (Exception _ex)
+            {
+                MessageBox.Show("Ha ocurrido un error: " + _ex.Message.ToString());
+                throw;
+            }           
+        }
+
+        public DataTable ActualizacionRegistros(DataTable dt) 
+        {
+            try
+            {
+                List<csr> listaActualizar = db.csr.OrderByDescending(x => x.id).Take(30000).ToList();
+
+                int filas = dt.Rows.Count;
+
+                foreach (DataRow orow in dt.Select())
+                {
+                    string referencia = orow["Referencia"].ToString();
+                    string ultimock = orow["Último Checkpoint"].ToString();
+
+                    var csrdata = listaActualizar.Where(x => x.Referencia.Contains(referencia)).FirstOrDefault();
+
+                    if (csrdata != null)
+                    {
+                        if (csrdata.UltimoCheckpoint != "OK")
+                        {
+                            string query = "UPDATE csr SET [UltimoCheckpoint] = @UltimoCheckpoINT WHERE ([Referencia] LIKE '%' + @Referencia + '%') AND ([UltimoCheckpoint] != 'OK')";
+                            db.Database.ExecuteSqlCommand(query, new SqlParameter("@Referencia", referencia), new SqlParameter("@UltimoCheckpoINT", ultimock));
+                            dt.Rows.Remove(orow);
+                        }
+                        else
+                        {
+                            dt.Rows.Remove(orow);
+                        }
+                    }
+                }
+
+                dt.AcceptChanges();
+
+                int filasRestante = dt.Rows.Count;
+
+                return dt;
+            }
+            catch (Exception _ex)
+            {
+                MessageBox.Show("Ha ocurrido un error: " + _ex.Message.ToString());
+                throw;
+            }
+        }       
+
+        public bool AgregarRegistrosNuevo(DataTable dt) 
+        {
+            int filas = dt.Rows.Count;
+
+            try
+            {
+                using (SqlConnection con = new SqlConnection("Data Source=sql7001.site4now.net; " + "Initial Catalog=DB_A3F19C_OG;" + "User id=DB_A3F19C_OG_admin;" + "Password=xQ9znAhU;"))
+                {
+                    using (SqlBulkCopy sqlBulkCopy = new SqlBulkCopy(con))
+                    {
+                        sqlBulkCopy.DestinationTableName = "dbo.csr";
+
+                        //Primer Parametro DataTable
+                        //Segundo Parametro BD
+                        sqlBulkCopy.ColumnMappings.Add("No de Cuenta", "NumeroCuenta");
+                        sqlBulkCopy.ColumnMappings.Add("Guía", "Guia");
+                        sqlBulkCopy.ColumnMappings.Add("Piece ID", "PieceID");
+                        sqlBulkCopy.ColumnMappings.Add("Referencia", "Referencia");
+                        sqlBulkCopy.ColumnMappings.Add("IATA Origen", "IATAOrigen");
+                        //sqlBulkCopy.ColumnMappings.Add("IATA Origen", "CiudadOrigen");
+                        sqlBulkCopy.ColumnMappings.Add("IATA Destino", "IATADestino");
+                        //sqlBulkCopy.ColumnMappings.Add("IATA Destino", "CiudadDestino");
+                        sqlBulkCopy.ColumnMappings.Add("SVC/SVP", "SVCSubIATA");
+                        sqlBulkCopy.ColumnMappings.Add("SVC/SVP", "Ruta");
+                        sqlBulkCopy.ColumnMappings.Add("Piezas", "Piezas");
+                        sqlBulkCopy.ColumnMappings.Add("Peso", "Peso");
+                        sqlBulkCopy.ColumnMappings.Add("Fecha Recolección ", "FechaRecoleccion");
+                        //sqlBulkCopy.ColumnMappings.Add("Fecha Recolecci�n ", "FechaPrimerCheckpointTerminal");
+                        sqlBulkCopy.ColumnMappings.Add("Hora Recolección ", "HoraPrimerCheckpointTerminal");
+                        //sqlBulkCopy.ColumnMappings.Add("�ltima Incidencia", "PrimerCheckpointTerminal");
+                        //sqlBulkCopy.ColumnMappings.Add("�ltima Incidencia", "DescripcionPrimerCheckTerminal");
+                        sqlBulkCopy.ColumnMappings.Add("Detalles de entrega / Comentarios", "DetallesEntregaComentarios");
+                        //sqlBulkCopy.ColumnMappings.Add("Intentos entrega", "TiempoTransitoEstimado");
+                        //sqlBulkCopy.ColumnMappings.Add("Intentos entrega", "TiempoTransitoRealizado");
+                        sqlBulkCopy.ColumnMappings.Add("Intentos entrega", "IntentosEntrega");
+                        //sqlBulkCopy.ColumnMappings.Add("Detalles de entrega / Comentarios", "CausaDemora");
+                        //sqlBulkCopy.ColumnMappings.Add("Fecha CC", "FechaIngresoCC");
+                        //sqlBulkCopy.ColumnMappings.Add("D�as en CC", "DiasCC");
+                        sqlBulkCopy.ColumnMappings.Add("Producto", "Producto");
+                        sqlBulkCopy.ColumnMappings.Add("Valor de Seguro", "ValorSeguro");
+                        sqlBulkCopy.ColumnMappings.Add("Nombre Remitente", "NombreRemitente");
+                        sqlBulkCopy.ColumnMappings.Add("Contacto Remitente", "ContactoRemitente");
+                        //sqlBulkCopy.ColumnMappings.Add("Direcci�n Remitente", "DireccionRemitente");
+                        sqlBulkCopy.ColumnMappings.Add("CP Remitente", "CPRemitente");
+                        sqlBulkCopy.ColumnMappings.Add("Nombre Destinatario", "NombreDestinatario");
+                        sqlBulkCopy.ColumnMappings.Add("Contacto Destinatario", "ContactoDestinatario");
+                        //sqlBulkCopy.ColumnMappings.Add("Direcci�n Destinatario", "DireccionDestinatario");
+                        sqlBulkCopy.ColumnMappings.Add("CP Destinatario", "CPDestinatario");
+                        sqlBulkCopy.ColumnMappings.Add("Último Checkpoint", "UltimoCheckpoint");
+                        //sqlBulkCopy.ColumnMappings.Add("Fecha del �ltimo checkpoint", "FechaUltimoCheckpoint");
+                        //sqlBulkCopy.ColumnMappings.Add("Hora del �ltimo checkpoint", "HoraUltimoCheckpoint");
+                        //sqlBulkCopy.ColumnMappings.Add("Detalles de entrega / Comentarios", "detalleultimocheckpoint");
+                        sqlBulkCopy.ColumnMappings.Add("FechaCarga", "FechaCarga");
+
+                        con.Open();
+
+                        sqlBulkCopy.WriteToServer(dt);
+
+                        con.Close();
+                    }
+                }
+            }
+            catch (Exception _ex)
+            {
+                MessageBox.Show("asd");
+            }        
+            
+            return false;
+        }
+    }
+}
